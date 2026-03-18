3f040e16a14980ddc99714c07729017600613d10
Randy Brown <thatrandybrown@gmail.com>
2026-03-17 20:42:49 -0500

    build the tamagui base

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
+import {
+  DarkTheme,
+  ThemeProvider
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { TamaguiProvider, Theme } from "tamagui";
+
+import { config } from "@tamagui/config";
+import { createTamagui } from "tamagui";
+
+const tamaguiConfig = createTamagui(config);
+
+type Conf = typeof tamaguiConfig;
+
+declare module "tamagui" {
+  interface TamaguiCustomConfig extends Conf {}
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme="dark">
+      <ThemeProvider value={DarkTheme}>
+        <Theme name="dark">
+          <Stack screenOptions={{ headerShown: false }} />
+        </Theme>
+      </ThemeProvider>
+    </TamaguiProvider>
+  );
 }
