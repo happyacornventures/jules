090375f0d8bf479dee4f24216a6924772759fb2a
Randy Brown <thatrandybrown@gmail.com>
2026-03-17 20:44:18 -0500

    send and receive messages to tauri

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { invoke } from "@tauri-apps/api/core";
+
+import {
+  BaseForm,
+  View
+} from "./component";
+
+// eventually this should be split based on platform as well--this is only true for web
+const dispatch = (event: string, payload: Record<string, unknown>) =>
+  window.__TAURI_INTERNALS__
+    ? invoke("dispatch", {
+        event,
+        payload: JSON.stringify(payload),
+      }).then((rsp) => JSON.parse(rsp as string))
+    : Promise.resolve({ node: {} });
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <BaseForm
+        schema={{
+          title: "Prompt",
+          properties: {
+            prompt: {
+              type: "string",
+              title: "prompt",
+              value: "",
+            },
+          },
+        }}
+        onSubmit={data => dispatch("form_submitted", data).then(console.log)}
+      />
     </View>
   );
 }
